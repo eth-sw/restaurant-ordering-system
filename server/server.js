ed0816d63c0e5d1acd1227a2971bc7858c49375f
ethsw <ets6@aber.ac.uk>
2026-01-31 17:08:24 +0000

    Configure MongoDB connection and server entry: Configured 'directConnection' to bypass University firewall. Connected to Primary Replica Set node manually.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
+require('dotenv').config(); // Load variables
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', require('./routes/auth'));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB Connected Successfully!'))
+  .catch(err => console.error('❌ MongoDB Connection Error:', err));
+
 app.get('/api/status', (req, res) => {
-    res.json({ message: "Backend is running successfully!" });
+    res.json({ message: "Backend is running and DB is connected!" });
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
